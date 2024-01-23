cb9629d53b7c522496a2b6886c95514155361589
Austin-Buchanan <austinbuchanan233@gmail.com>
2024-01-23 16:36:47 -0600

    Worked more on intro gui

--- main.py
+++ main.py
@@ -17,22 +17,45 @@ import PySimpleGUI as sg
 
 # window.close()
 
-def intro(positions):
-    layout = [
-        [sg.Text("Let's play bridge!\nEnter you name: ")],
-        [sg.Input('', enable_events=True, key='-InputName-', expand_x=True)]
-    ]
-    window = sg.Window("Demo", layout)
+# def intro(positions):
+#     layout = [
+#         [sg.Text("Let's play bridge!\nEnter you name: ")],
+#         [sg.Input('', enable_events=True, key='-InputName-', expand_x=True)]
+#     ]
+#     window = sg.Window("Demo", layout)
 
-    while True:
-        event, values = window.read()
-        if event == "OK" or event == sg.WIN_CLOSED:
-            break
+#     while True:
+#         event, values = window.read()
+#         if event == "OK" or event == sg.WIN_CLOSED:
+#             break
 
 def main():
     mainTable = Table([])
     positions = list(mainTable.positions.keys())
-    username, userPosition = intro(positions)
+    # username, userPosition = intro(positions)
+
+    sg.theme('BluePurple')
+    layout = [[sg.Text("Let's play bridge!\nEnter your name: "), sg.Text(size=(15,1), key='-INTRO-')],
+              [sg.Input(key='-NAME-')],
+              [sg.Text('Where would you like to sit?'), sg.Text(size=(15,1), key='-QPOSITION-')],
+              [sg.Combo(positions, key='-POSITION-')],
+              [sg.Button('Start')],
+              [sg.Text(''), sg.Text( key='-ERRORTEXT-')],
+              [sg.Button('Exit')]]
+    
+    window = sg.Window('Austin Codes Bridge', layout)
+
+    while True: 
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Start':
+            if values['-NAME-'] != '' and values['-POSITION-'] != '':
+                window['-ERRORTEXT-'].update('No error detected.')
+            else:
+                window['-ERRORTEXT-'].update('Please enter your name and select a position before clicking Start.')
+
 
 if __name__ == "__main__":
     main()
