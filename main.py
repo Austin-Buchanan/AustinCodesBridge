8778463846753a9564bba1bc9f7bf2394833fb1d
Austin-Buchanan <austinbuchanan233@gmail.com>
2024-01-24 07:31:38 -0600

    Made intro layout invisible after clicking start

--- main.py
+++ main.py
@@ -7,41 +7,20 @@ from trick import Trick
 import random, sys
 import PySimpleGUI as sg 
 
-# layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-# window = sg.Window("Demo", layout)
-
-# while True:
-#     event, values = window.read()
-#     if event == "OK" or event ==sg.WIN_CLOSED:
-#         break
-
-# window.close()
-
-# def intro(positions):
-#     layout = [
-#         [sg.Text("Let's play bridge!\nEnter you name: ")],
-#         [sg.Input('', enable_events=True, key='-InputName-', expand_x=True)]
-#     ]
-#     window = sg.Window("Demo", layout)
-
-#     while True:
-#         event, values = window.read()
-#         if event == "OK" or event == sg.WIN_CLOSED:
-#             break
-
 def main():
     mainTable = Table([])
     positions = list(mainTable.positions.keys())
-    # username, userPosition = intro(positions)
 
     sg.theme('BluePurple')
-    layout = [[sg.Text("Let's play bridge!\nEnter your name: "), sg.Text(size=(15,1), key='-INTRO-')],
+    layout_intro = [[sg.Text("Let's play bridge!\nEnter your name: "), sg.Text(key='-INTRO-')],
               [sg.Input(key='-NAME-')],
-              [sg.Text('Where would you like to sit?'), sg.Text(size=(15,1), key='-QPOSITION-')],
+              [sg.Text('Where would you like to sit?'), sg.Text(key='-QPOSITION-')],
               [sg.Combo(positions, key='-POSITION-')],
               [sg.Button('Start')],
-              [sg.Text(''), sg.Text( key='-ERRORTEXT-')],
-              [sg.Button('Exit')]]
+              [sg.Text(''), sg.Text( key='-ERRORTEXT-')]]
+    
+    layout = [[sg.Column(layout_intro, key='-COLINTRO-')],
+               [sg.Button('Exit')]]
     
     window = sg.Window('Austin Codes Bridge', layout)
 
@@ -51,10 +30,10 @@ def main():
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Start':
-            if values['-NAME-'] != '' and values['-POSITION-'] != '':
-                window['-ERRORTEXT-'].update('No error detected.')
+            if values['-NAME-'] != '' and values['-POSITION-'] in positions:
+                window['-COLINTRO-'].update(visible=False)
             else:
-                window['-ERRORTEXT-'].update('Please enter your name and select a position before clicking Start.')
+                window['-ERRORTEXT-'].update('Please enter your name and select a position from the dropdown before clicking Start.')
 
 
 if __name__ == "__main__":
